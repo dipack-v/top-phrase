56ef82ea0a27e006b3e6c5dba72face14f8ca1ef
Deepak <dipack.v@gmail.com>
2017-05-15 10:47:16 +0530

    code clean up

--- src/test/java/com/example/TopPhrasesTests.java
+++ src/test/java/com/example/TopPhrasesTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BootBatchApplicationTests {
+public class TopPhrasesTests {
 
 	@Test
 	public void contextLoads() {
